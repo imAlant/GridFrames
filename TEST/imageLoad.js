f651de8bf0b323e810b7c4d5ddf2b09ed7dd4279
imAlant <alantsabu33@outlook.com>
2021-07-27 01:13:45 +0530

    july26th commit

--- TEST/imageLoad.js
+++ TEST/imageLoad.js
@@ -1,5 +1,7 @@
 $(function () {
 
+    var imagePath;
+
     $('#img-upload').on('change', function (event) {
 
         for(var i =0 ;  i<this.files.length ; i++){
@@ -8,7 +10,7 @@ $(function () {
             var path = URL.createObjectURL(event.target.files[i]);
             images.push(path);
             $('<div class="flex-fill me-2 mb-3 inner-image-container">'+
-            '<img src='+path+' alt="no image">'+
+            '<img src='+path+' alt="no image"  width="180%" height="180%">'+
             '<div class="settings" id = '+path+'>'+
                 '<i class="fas fa-crop crop" data-bs-toggle="modal" data-bs-target="#imageModal"></i>'+
                 '<i class="fas fa-trash delete"></i>'+
@@ -18,18 +20,29 @@ $(function () {
 
     // Add image to the modal
 
-   $("body").on('click','.crop',function (event) {
-       var imagePath = $(this).parent().attr('id');
+   $("body").on('click','.crop',function () {
+       imagePath = $(this).parent().attr('id');
        $('.modal-contents img').attr('src',imagePath);
      });
 
     //  Drag and move the image inside the modal.
 
     $('.modal-contents').mousedown(function () { 
+        // console.log('mouse down')
         $('.modal-contents').mousemove(function (e) { 
             // values: e.clientX, e.clientY, e.pageX, e.pageY
-            console.log('x: '+e.clientX, 'y: '+e.clientY)
+            // console.log('x: '+e.clientX, 'y: '+e.clientY)
+            initX = e.clientX;
+            initY = e.clientY;
+
         });
     });
+
+    // Size controll slider
+
+    $('body').on('click','#sample',function () {
+        var sample = $('#sample').parent().siblings('.modal-contents').children('img').attr('height');
+        alert(sample)
+      });
 
 });
