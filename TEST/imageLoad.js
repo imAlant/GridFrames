4675614af8a1b25f0b648c1aa342edd14256af89
imAlant <alantsabu33@outlook.com>
2021-07-29 02:44:43 +0530

    28th july commit

--- TEST/imageLoad.js
+++ TEST/imageLoad.js
@@ -3,7 +3,8 @@ $(function () {
     var imagePath;
     var width;
     var height;
-
+    var size;
+    const sliderVal= {};
 
     $('#img-upload').on('change', function (event) {
 
@@ -11,10 +12,14 @@ $(function () {
 
             var images = [];
             var path = URL.createObjectURL(event.target.files[i]);
-            images.push(path);
+            var imageUniqueId = path.split('/')[3];
+            images.push(imageUniqueId);
+            sliderVal[imageUniqueId] = {'sliderValue':0};
+            console.log(sliderVal[imageUniqueId].sliderValue);
+
             $('<div class="flex-fill me-2 mb-3 inner-image-container">' +
-                '<img src=' + path + ' alt="no image"  width="100%" height="100%">' +
-                '<div class="settings" id = ' + path + '>' +
+                '<img id=' + imageUniqueId + ' src=' + path + ' alt="no image"  width="100%" height="100%">' +
+                '<div class="settings">' +
                 '<i class="fas fa-crop crop" data-bs-toggle="modal" data-bs-target="#imageModal"></i>' +
                 '<i class="fas fa-trash delete"></i>' +
                 '</div></div>').insertBefore('.input-control');
@@ -24,12 +29,14 @@ $(function () {
     // Add image to the modal
 
     $("body").on('click', '.crop', function () {
-        imagePath = $(this).parent().attr('id');
-        modalImgId = imagePath.split('/')[3];
+        imagePath = $(this).parent().siblings('img').attr('src');
+        modalImgId = $(this).parent().siblings('img').attr('id');
         $('.modal-contents img').attr('src', imagePath);
-        $('.modal-contents img').attr('id',modalImgId);
+        $('.modal-contents img').attr('class', modalImgId);
         $('.modal-contents img').width('100%');
         $('.modal-contents img').height('100%');
+
+        $('#size-slider').val(sliderVal[modalImgId].sliderValue);
 
     });
 
@@ -37,14 +44,9 @@ $(function () {
 
     $('#imageModal').on('show.bs.modal', function () {
         setTimeout(function () {
-        //     // $('#size-slider').val(90);
-        //     var size = $('#size-slider').val();
-        //     // alert(size);
-        //     // console.log('width: ' + (parseInt(width) + parseInt(size)) + '  height: ' + (parseInt(height) + parseInt(size)));
-        //     $('#size-slider').parent().siblings('.modal-contents').children('#'+modalImgId).height((parseInt(height) + parseInt(size * 2)) + 'px');
-        //     $('#size-slider').parent().siblings('.modal-contents').children('#'+modalImgId).width((parseInt(width) + parseInt(size * 2)) + 'px');
             width = $('.modal-contents img').width();
             height = $('.modal-contents img').height();
+            // console.log(height,width);
         }, 500);
 
     });
@@ -52,7 +54,6 @@ $(function () {
     //  Drag and move the image inside the modal.
 
     $('.modal-contents').mousedown(function () {
-        // console.log('mouse down')
         $('.modal-contents').mousemove(function (e) {
             // values: e.clientX, e.clientY, e.pageX, e.pageY
             // console.log('x: '+e.clientX, 'y: '+e.clientY)
@@ -65,12 +66,18 @@ $(function () {
     // Size controll slider
 
     $('#size-slider').on('input', function () {
-        var size = $('#size-slider').val();
-        // console.log('width: ' + (parseInt(width) + parseInt(size)) + '  height: ' + (parseInt(height) + parseInt(size)));
-        $('#size-slider').parent().siblings('.modal-contents').children('#'+modalImgId).height((parseInt(height) + parseInt(size * 2)) + 'px');
-        $('#size-slider').parent().siblings('.modal-contents').children('#'+modalImgId).width((parseInt(width) + parseInt(size * 2)) + 'px');
-        // modalSliderValueDict[modalImgId]=size.toString();
-        // console.log(modalSliderValueDict.modalImgId);
+        size = $('#size-slider').val();
+        // console.log(modalImgId);
+        // $('#size-slider').parent().siblings('.modal-contents').children('#' + modalImgId).height((parseInt(height) + parseInt(size * 2)) + 'px');
+        // $('#size-slider').parent().siblings('.modal-contents').children('#' + modalImgId).width((parseInt(width) + parseInt(size * 2)) + 'px');
+        $('.'+modalImgId).height((height+(size*2))+'px');
+        $('.'+modalImgId).width((width+(size*2))+'px');
+    });
+
+    // save button
+
+    $('#save-changes').on('click', function () {
+        sliderVal[modalImgId] = {'sliderValue':$('#size-slider').val()};
     });
 
 });
