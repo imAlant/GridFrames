de33c59cf150076827ee3d3d3e80322f3b434f51
imAlant <alantsabu33@outlook.com>
2021-07-31 02:28:57 +0530

    30th july commit

--- TEST/imageLoad.js
+++ TEST/imageLoad.js
@@ -1,5 +1,8 @@
 $(function () {
 
+    $('img').on('dragstart', function(event) { event.preventDefault(); });
+
+
     var imagePath;
     var width;
     var height;
@@ -17,7 +20,7 @@ $(function () {
             sliderVal[imageUniqueId] = {'sliderValue':0};
             console.log(sliderVal[imageUniqueId].sliderValue);
 
-            $('<div class="flex-fill me-2 mb-3 inner-image-container">' +
+            $('<div class="flex-fill me-2 mb-3 inner-image-container container-frame">' +
                 '<img id=' + imageUniqueId + ' src=' + path + ' alt="no image"  width="100%" height="100%">' +
                 '<div class="settings">' +
                 '<i class="fas fa-crop crop" data-bs-toggle="modal" data-bs-target="#imageModal"></i>' +
@@ -35,7 +38,6 @@ $(function () {
         $('.modal-contents img').attr('class', modalImgId);
         $('.modal-contents img').width('100%');
         $('.modal-contents img').height('100%');
-
         $('#size-slider').val(sliderVal[modalImgId].sliderValue);
 
     });
@@ -53,15 +55,35 @@ $(function () {
 
     //  Drag and move the image inside the modal.
 
-    $('.modal-contents').mousedown(function () {
-        $('.modal-contents').mousemove(function (e) {
-            // values: e.clientX, e.clientY, e.pageX, e.pageY
-            // console.log('x: '+e.clientX, 'y: '+e.clientY)
-            initX = e.clientX;
-            initY = e.clientY;
+    $('.modal-contents').on('mousedown',function (event) { 
+        var pos = $('.'+modalImgId).position();
+        var leftPos = pos.left;
+        var topPos = pos.top;
+        var currentX = event.clientX;
+        var currentY = event.clientY;
+        $('.modal-contents').on('mousemove',function (e) {
 
+            if(e.clientX > currentX){
+                $('.'+modalImgId).css({left: leftPos++ +'px'})
+            }
+
+            if(e.clientX < currentX){
+                $('.'+modalImgId).css({left: leftPos-- +'px'})
+            }
+
+            if(e.clientY > currentY){
+                $('.'+modalImgId).css({top: topPos++ +'px'})
+            }
+
+            if(e.clientY < currentY){
+                $('.'+modalImgId).css({top: topPos-- +'px'})
+            }
         });
-    });
+      });
+
+    $('.modal-contents').on('mouseup',function () {
+        $('.modal-contents').off('mousemove');
+      });
 
     // Size controll slider
 
